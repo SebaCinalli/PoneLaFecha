d02339d14d95b8c5f04b5604e3766cd425ebb89e
nanosalvu11 <salvuccinahuel@gmail.com>
2025-08-03 12:38:40 -0300

    se agregaron botones para la navegacion desde el menu principal con las rutas correspondientes y se creo el SalonController

---
+++ UI.Web/Controllers/SalonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Entidades;
+using Negocio;
+
+namespace UI.Web.Controllers
+{
+    public class SalonController : Controller
+    {
+        public IActionResult Index() => View(LogicaSalon.Listar());
+
+        public IActionResult Crear() => View();
+
+        [HttpPost]
+        public IActionResult Crear(Salon s)
+        {
+            LogicaSalon.Crear(s);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var salon = LogicaSalon.Obtener(id);
+            return salon == null ? NotFound() : View(salon);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Salon s)
+        {
+            LogicaSalon.Editar(s);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            LogicaSalon.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
